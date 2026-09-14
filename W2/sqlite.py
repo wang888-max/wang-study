8af9f6d557b55f79f0b79887c3ddbf66d2d277ce
wang <2577757573@qq.com>
2026-09-14 20:31:15 +0800

    sqlite基础学习

---
+++ W2/sqlite.py
@@ -0,0 +1,22 @@
+import sqlite3
+con = sqlite3.connect("tutorial.db")
+cur = con.cursor()
+# cur.execute("CREATE TABLE movie(title, year, score)")
+cur.execute("""
+    INSERT INTO movie VALUES
+        ('Monty Python and the Holy Grail', 1975, 8.2),
+        ('And Now for Something Completely Different', 1971, 7.5)
+""")
+con.commit()
+
+data = [
+    ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+    ("Monty Python's The Meaning of Life", 1983, 7.5),
+    ("Monty Python's Life of Brian", 1979, 8.0),
+]
+cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+con.commit()  # 记得在执行 INSERT 之后提交事务。
+
+
+for row in cur.execute("SELECT year, title FROM movie ORDER BY year"):
+    print(row)
---
+++ W2/异常进阶.py
@@ -0,0 +1,15 @@
+# try:
+#     raise NameError('HiThere')
+# except NameError:
+#     print('An exception flew by!')
+#     raise
+
+import sys
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()
